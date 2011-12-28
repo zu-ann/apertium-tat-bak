152b7eab61c1340164ac9961c234e96326a73d72
Jonathan North Washington <jonathan.n.washington@gmail.com>
2011-12-28 05:50:55 +0000

    some scripts to count stems

---
+++ countstems-ba.sh
@@ -0,0 +1,26 @@
+lexc="apertium-tt-ba.ba.lexc"
+allcounts=`hfst-lexc --format foma $lexc -o /dev/null | grep Root | sed 's/,/\n/g'`
+
+for line in $allcounts; do
+	thing=`echo $line | sed -r 's/(.*)\.\.\.([0-9]*),?/\1/'`;
+	numbr=`echo $line | sed -r 's/(.*)\.\.\.([0-9]*),?/\2/'`;
+	if [[ "$thing" == "Root" ]]; then
+		rootsize=$numbr;
+		root=`grep -A$rootsize Root $lexc | grep -v Root`;
+	fi;
+done;
+
+root=`echo $root | sed -r 's/\s*;/\n/g'`;
+
+string=""
+for line in $allcounts; do
+	thing=`echo $line | sed -r 's/(.*)\.\.\.([0-9]*),?/\1/'`;
+	numbr=`echo $line | sed -r 's/(.*)\.\.\.([0-9]*),?/\2/'`;
+	for thingname in $root; do
+		if [ "$thing" = "$thingname" ]; then
+			string="$string + $numbr";
+		fi;
+	done;
+done;
+
+echo `calc $string`;
